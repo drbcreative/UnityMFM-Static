da8c359b9e70723421a58159b69c072580fe7889
Danny <dannybcreative@gmail.com>
2019-06-18 11:22:29 -0700

    diagnostic section is complete but still needs arrows for carousel

--- js/slider-init.js
+++ js/slider-init.js
@@ -1,11 +1,37 @@
 $(document).ready(function () {
-  $("").slick({
-    slidesToShow: 1,
+  $(".dx-slider").slick({
+    slidesToShow: 3,
     slidesToScroll: 1,
     infinite: !0,
-    dots: true,
+    dots: false,
     autoplay: true,
-    arrows: false,
-    autoplaySpeed: 5000
+    arrows: true,
+    autoplaySpeed: 5000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
   });
 });
